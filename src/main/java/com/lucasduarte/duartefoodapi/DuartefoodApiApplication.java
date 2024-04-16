5a206229ac73d3ef4f29d935afab5874da6b3778
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-04-15 21:02:52 -0300

    Projeto iniciado e tabelas cozinha e restaurante também

---
+++ src/main/java/com/lucasduarte/duartefoodapi/DuartefoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucasduarte.duartefoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuartefoodApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuartefoodApiApplication.class, args);
+    }
+
+}
