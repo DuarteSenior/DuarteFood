f7673c06273957380c2a450022c6899509f7aa7f
DuarteProg <lucas.duartebarbosa@outlook.com>
2024-04-16 18:42:15 -0300

    metodo http da busca por id criado

--- src/main/java/com/lucasduarte/duartefoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/lucasduarte/duartefoodapi/api/controller/CozinhaController.java
@@ -3,7 +3,9 @@ package com.lucasduarte.duartefoodapi.api.controller;
 import com.lucasduarte.duartefoodapi.domain.model.Cozinha;
 import com.lucasduarte.duartefoodapi.infrastructure.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,12 +19,17 @@ public class CozinhaController {
     private CozinhaRepository cozinhaRepository;
 
     @GetMapping
-    public List<Cozinha> listar(){
+    public List<Cozinha> listar() {
         return cozinhaRepository.findAll();
     }
 
     @GetMapping("/{cozinhaId}")
-    public Optional<Cozinha> buscar(@PathVariable Long cozinhaId){
-        return cozinhaRepository.findById(cozinhaId);
+    public ResponseEntity<Optional<Cozinha>> buscar(@PathVariable Long cozinhaId) {
+        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+
+        if (cozinha.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+        }
+        return ResponseEntity.notFound().build();
     }
 }
